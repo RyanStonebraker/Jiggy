54a412eb17690eabd83311df668039d2b85da250
rawessels <rawessels@alaska.edu>
2017-04-18 11:10:20 -0800

    Traingle File work nothing too serious

--- JIGTriangle.h
+++ JIGTriangle.h
@@ -7,13 +7,24 @@ JIGTriangle.h
 */
 #ifndef JIGTriangle_H_
 #define JIGTriangle_H_
+#include "JIGShape.h"
 namespace JIG{
-	class Triangle {
+	class Triangle : public Shape {
 	public:
-	private:
-		float _length;
+		Triangle(KFAPoint centerPoint, float side, float rotationAngle, KFAColorRGBA color, int vaID = -1);
+		Triangle(const Triangle &copyTri);
+		/*void virtual submitForRender(void);
+		void virtual updateGeometry(int horizontalVelocity, int verticalVelocity, float rotation);
+		void virtual updateGeometry(int horizontalVelocity, int verticalVelocity, float rotation, KFAColorRGBA color);
+		~Triangle();*/
+
+	protected:
+		float _side;
+
+		//void virtual createDataBlock(void);
 
 	};
+
 }
 #endif /*JIGTriangle_H_*/
 
