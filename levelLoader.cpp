7b361626f493f3e1c5d2f46a6ea858265ffcf61c
Ryan Stonebraker <ryanastonebraker@gmail.com>
2017-04-25 10:44:50 -0800

    Almost working level loader

--- levelLoader.cpp
+++ levelLoader.cpp
@@ -1,6 +1,6 @@
 // levelLoader.cpp
 // Ryan Stonebraker
-// 4/16/2017
+// Last Modified: 4/25/2017
 // loads shape information from text file
 
 #include "levelLoader.h"
@@ -15,14 +15,20 @@
 #include "KFAGeometry.h"
 #include <vector>
 #include <memory>
+#include <tuple>
 
 JIG::LoadLevel::LoadLevel(std::string fname) : _fname{fname} {
     _getFromText();
 }
 
 void JIG::LoadLevel::_getFromText() {
+//    KFAColorRGBA blue = JIGMakeColor(0.0f, 0.38f, 1.0f, 1.0f);
+//    KFAColorRGBA red = JIGMakeColor(1.0f, 0.2f, 0.0f, 1.0f);
+    KFAColorRGBA grey = JIGMakeColor(0.717f, 0.717f, 0.717f, 1.0f);
+
+    
     // TODO ERROR CHECKING
-    std::ifstream reader(_fname);
+    std::ifstream reader(this->_fname);
     
     std::string shapeData;
 
@@ -39,11 +45,14 @@ void JIG::LoadLevel::_getFromText() {
         float width = 0, height = 0;
         shapeParser >> width >> height;
         
-        float angle = 1;
+        float angle = 0;
         shapeParser >> angle;
         
-        if (shapeType == "Rect")
-            _loadedShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(shapeLoc, width, height, 0.0f, JIGMakeColor(1.0f, 1.0f, 1.0f, 1.0f))));
+        if (shapeType == "Rect") {
+            _loadedShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(shapeLoc, width, height, angle, grey)));
+            _loadedShapeInfo.push_back(std::make_tuple("Rect", shapeLoc, width, height, angle));
+        }
+        
     }
 }
 
@@ -51,9 +60,80 @@ std::unique_ptr<JIG::Shape> & JIG::LoadLevel::operator[](int i) {
     return _loadedShapes[i];
 }
 
-//void JIG::LoadLevel::renderLevel() const {
-//    for (unsigned i = 0; i < _loadedShapes.size(); ++i) {
-//        _loadedShapes[i]->submitForRender();
-//    }
+void JIG::LoadLevel::renderLevel() const {
+    for (unsigned i = 0; i < _loadedShapes.size(); ++i) {
+        _loadedShapes[i]->submitForRender();
+    }
 
+}
+
+std::string JIG::LoadLevel::_getString(int loc) const {
+    std::string shapeStr = std::get<JIG::LoadLevel::NAME>(_loadedShapeInfo[loc]) + " ";
+    
+    std::ostringstream holdPos;
+    auto position = this->location(loc);
+    holdPos << position.x;
+    shapeStr += holdPos.str() + " ";
+    
+    holdPos.str("");
+    holdPos.clear();
+    
+    holdPos << position.y;
+    shapeStr += holdPos.str() + " ";
+    
+    holdPos.str("");
+    holdPos.clear();
+    
+    holdPos << position.z;
+    shapeStr += holdPos.str() + " ";
+    
+    std::ostringstream holdAng;
+    holdAng << this->rotation(loc);
+    
+    shapeStr += holdAng.str();
+    
+    return shapeStr;
+}
+
+std::string JIG::LoadLevel::_getTotalString() const {
+    std::string allShapesStr;
+    for (unsigned i = 0; i < _loadedShapes.size(); ++i)
+        allShapesStr += _getString(i) + '\n';
+    return allShapesStr;
+}
+
+std::ostream &JIG::operator<<(std::ostream& out, LoadLevel& rhs) {
+    return out << rhs._getTotalString();
+}
+
+void JIG::LoadLevel::updateFile() {
+    this->updateFile(_fname);
+}
+void JIG::LoadLevel::updateFile(std::string fname) {
+    std::ofstream writer (fname);
+    writer << this->_getTotalString();
+}
+
+//KFAColorRGBA &color(int loc) {
+//    
 //}
+//
+//KFAColorRGBA color(int loc) const {
+//    
+//}
+
+KFAPoint &JIG::LoadLevel::location(int loc) {
+    return std::get<JIG::LoadLevel::POSITION>(_loadedShapeInfo[loc]);
+}
+
+KFAPoint JIG::LoadLevel::location(int loc) const {
+    return std::get<JIG::LoadLevel::POSITION>(_loadedShapeInfo[loc]);
+}
+
+float &JIG::LoadLevel::rotation(int loc) {
+    return std::get<JIG::LoadLevel::ANGLE>(_loadedShapeInfo[loc]);
+}
+
+float JIG::LoadLevel::rotation(int loc) const {
+    return std::get<JIG::LoadLevel::ANGLE>(_loadedShapeInfo[loc]);
+}
--- levelLoader.h
+++ levelLoader.h
@@ -1,6 +1,6 @@
 // levelLoader.h
 // Ryan Stonebraker
-// 4/16/2017
+// Last Modified: 4/25/2017
 // header for cpp that loads shape information from text file
 
 #ifndef LEVEL_LOADER_H
@@ -14,30 +14,40 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <iostream>
+#include <tuple>
 
 namespace JIG {
     class LoadLevel {
     public:
-        LoadLevel(std::string); // takes file name // submit all shapes for rendering to get vtx array ids
-        std::unique_ptr<JIG::Shape> &operator[](int); // identifier = vtx array id
+        friend std::ostream &operator<<(std::ostream&, LoadLevel&);
         
-        void updateFile() const;
+        LoadLevel(std::string); // takes file name // submit all shapes for rendering to get vtx array ids
+        std::unique_ptr<JIG::Shape> &operator[](int); // identifier = order in which loaded/vtx array id
+        
+        void updateFile();
         void updateFile(std::string);
         void renderLevel() const;
-        KFAColorRGBA &color(int);
+   
         KFAPoint &location(int);
+        KFAPoint location(int) const;
         float &rotation(int);
+        float rotation(int) const;
         
     private:
-        std::vector <std::unique_ptr<JIG::Shape>> _loadedShapes; // vAID, ptr to shape
+        enum {NAME, POSITION, WIDTH, HEIGHT, ANGLE};
+        std::vector <std::unique_ptr<JIG::Shape>> _loadedShapes;
+        std::vector <std::tuple<std::string, KFAPoint, int, int, float> > _loadedShapeInfo;
         std::string _fname;
-        // std::unique_ptr<JIG::Shape> _createShape(std::string, KFAPoint, float, float, float);
+        std::string _getString(int) const;
+        std::string _getTotalString() const;
+        
         void _getFromText();
     };
     
 //    extern KFAColorRGBA blue = JIGMakeColor(0.0f, 0.38f, 1.0f, 1.0f);
 //    extern KFAColorRGBA red = JIGMakeColor(1.0f, 0.2f, 0.0f, 1.0f);
-//    extern KFAColorRGBA grey = JIGMakeColor(0.717f, 0.717f, 0.717f);
+//    extern KFAColorRGBA grey = JIGMakeColor(0.717f, 0.717f, 0.717f, 1.0f);
     
     
 } // namespace JIG
