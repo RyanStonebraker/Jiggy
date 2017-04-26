98ec7f027ec69b25fe35557718019723b81efe61
rawessels <rawessels@alaska.edu>
2017-04-26 00:54:01 -0800

    Triangle file needs datablock added to it

--- JIGTriangle.cpp
+++ JIGTriangle.cpp
@@ -8,5 +8,46 @@ JIGTriangle.cpp
 */
 #include "JIGTriangle.h"
 namespace JIG{
-	
+	Triangle::Triangle(KFAPoint centerPoint, float length,float rotationAngle, KFAColorRGBA color, int vaID)
+	{
+		_width = length;
+		_height = length;
+		_length = length;
+		_centerPoint = centerPoint;
+		_color = color;
+		_rotation = rotationAngle;
+		this->vertexArrayID = vaID;
+		this->_drawDataSize = 0;
+
+
+	}
+	Triangle::Triangle(const Triangle & tri)
+	{
+		this->_length = tri._length;
+		this->_centerPoint = tri._centerPoint;
+		this->_color = tri._color;
+		this->_drawDataSize = tri._drawDataSize;
+		this->drawData = tri.drawData;
+		this->_rotation = tri._rotation;
+		this->vertexArrayID = tri.vertexArrayID;
+	}
+	void Triangle::submitForRender(void)
+	{
+		if (this->_drawDataSize != 0) {
+			free(this->drawData);
+			this->_drawDataSize = 0;
+		}
+		this->createDataBlock();
+
+		if (this->vertexArrayID == -1) {
+			this->vertexArrayID = OGL_AssignMemoryToVertexArray(this->drawData, kfaGeometryTypeRectangle, this->_drawDataSize, -1);
+		}
+		else {
+			OGL_AssignMemoryToVertexArray(this->drawData, kfaGeometryTypeRectangle, this->_drawDataSize, this->vertexArrayID);
+		}
+	}
+	Triangle::~Triangle()
+	{
+		OGL_DisposeVertexArray(this->vertexArrayID);
+	}
 }
--- JIGTriangle.h
+++ JIGTriangle.h
@@ -7,10 +7,18 @@ JIGTriangle.h
 */
 #ifndef JIGTriangle_H_
 #define JIGTriangle_H_
+#include "JIGShape.h"
 namespace JIG{
-	class Triangle {
+	class Triangle: public Shape {
 	public:
-	private:
+		Triangle(KFAPoint centerPoint, float length,float rotationAngle, KFAColorRGBA color, int vaID = -1);
+		Triangle(const Triangle &tri);
+		void virtual submitForRender(void);
+		void virtual updateGeometry(int horizontalVelocity, int verticalVelocity, float rotation);
+		void virtual updateGeometry(int horizontalVelocity, int verticalVelocity, float rotation, KFAColorRGBA color);
+		~Triangle();
+	protected:
+		void virtual createDataBlock(void);
 		float _length;
 
 	};
