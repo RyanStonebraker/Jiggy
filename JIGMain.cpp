41d8314ed74e858ded92805f531b4c48479817ca
tvancise <tvancise@alaska.edu>
2017-04-26 23:00:14 -0800

    Shatter Proto

--- JIGMain.cpp
+++ JIGMain.cpp
@@ -7,18 +7,18 @@
 //
 
 #if __APPLE__
-    #include <Carbon/Carbon.h>
-    #include <OpenGL/OpenGL.h>
-    #include <OpenGL/gl.h>
+#include <Carbon/Carbon.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
 #elif _WIN32
-	#ifndef WIN32_LEAN_AND_MEAN
-		#define WIN32_LEAN_AND_MEAN
-	#endif
-	#define KB_MOST_SIGNIFICANT_BIT 0x8000
-	#include <windows.h>
-	#include <stdlib.h>
-	#include <iostream>
-	#pragma comment(lib,"user32.lib")
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#define KB_MOST_SIGNIFICANT_BIT 0x8000
+#include <windows.h>
+#include <stdlib.h>
+#include <iostream>
+#pragma comment(lib,"user32.lib")
 #endif
 
 #include "KFAData.h"
@@ -28,54 +28,56 @@
 #include "JIGArcSlice.h"
 #include "JIGCollisionDetector.h"
 #include "levelLoader.h"
+#include <random>
 
 std::vector<std::unique_ptr<JIG::Shape>>global_LevelShapes;
 int iterator = 0;
+bool first = true;
 
 /***************************************************/
 //DEMO CODE - DELETE ON FINAL RELEASE
 /***************************************************/
 //start
-std::vector<KFAPoint> demo = { KFAPoint{300,300,200}, KFAPoint{-300,-300,200}, KFAPoint{-300,300,200}, 
-							   KFAPoint{300,-300,200}, KFAPoint{0,0,200}, KFAPoint{100,100,200} };
+std::vector<KFAPoint> demo = { KFAPoint{ 300,300,200 }, KFAPoint{ -300,-300,200 }, KFAPoint{ -300,300,200 },
+KFAPoint{ 300,-300,200 }, KFAPoint{ 0,0,200 }, KFAPoint{ 100,100,200 } };
 //end
 #if __APPLE__
 void HandleKeypresses(void)
 {
-    static bool shouldChangeColor = NO;
-    int horizVelocity = 0, vertVelocity = 0, rotation = 0;
-    
-    if (pressedKeys[kVK_ANSI_W]) {
-        vertVelocity += 10;
-    }
-    if (pressedKeys[kVK_ANSI_A]) {
-        horizVelocity -= 10;
-    }
-    if (pressedKeys[kVK_ANSI_S]) {
-        vertVelocity -= 10;
-    }
-    if (pressedKeys[kVK_ANSI_D]) {
-        horizVelocity += 10;
-    }
-    if (pressedKeys[kVK_ANSI_C]) {
-        shouldChangeColor = !shouldChangeColor;
-    }
-    if (pressedKeys[kVK_ANSI_C]) {
-        rotation -= 5;
-    }
-    if (pressedKeys[kVK_ANSI_V]) {
-        rotation += 5;
-    }
+	static bool shouldChangeColor = NO;
+	int horizVelocity = 0, vertVelocity = 0, rotation = 0;
 
-    
-    global_LevelShapes[0]->updateGeometry(horizVelocity, vertVelocity, rotation);
-    
-    if (pressedKeys[kVK_ANSI_I]) {
-        OGL_AdjustCamera((float[]){KFA_ZOOM_SPEED, KFA_ZOOM_SPEED, KFA_ZOOM_SPEED}, KFA_CAMERA_ZOOM);
-    }
-    if (pressedKeys[kVK_ANSI_K]) {
-        OGL_AdjustCamera((float[]){-KFA_ZOOM_SPEED, -KFA_ZOOM_SPEED, -KFA_ZOOM_SPEED}, KFA_CAMERA_ZOOM);
-    }
+	if (pressedKeys[kVK_ANSI_W]) {
+		vertVelocity += 10;
+	}
+	if (pressedKeys[kVK_ANSI_A]) {
+		horizVelocity -= 10;
+	}
+	if (pressedKeys[kVK_ANSI_S]) {
+		vertVelocity -= 10;
+	}
+	if (pressedKeys[kVK_ANSI_D]) {
+		horizVelocity += 10;
+	}
+	if (pressedKeys[kVK_ANSI_C]) {
+		shouldChangeColor = !shouldChangeColor;
+	}
+	if (pressedKeys[kVK_ANSI_C]) {
+		rotation -= 5;
+	}
+	if (pressedKeys[kVK_ANSI_V]) {
+		rotation += 5;
+	}
+
+
+	global_LevelShapes[0]->updateGeometry(horizVelocity, vertVelocity, rotation);
+
+	if (pressedKeys[kVK_ANSI_I]) {
+		OGL_AdjustCamera((float[]) { KFA_ZOOM_SPEED, KFA_ZOOM_SPEED, KFA_ZOOM_SPEED }, KFA_CAMERA_ZOOM);
+	}
+	if (pressedKeys[kVK_ANSI_K]) {
+		OGL_AdjustCamera((float[]) { -KFA_ZOOM_SPEED, -KFA_ZOOM_SPEED, -KFA_ZOOM_SPEED }, KFA_CAMERA_ZOOM);
+	}
 }
 #elif _WIN32
 
@@ -109,17 +111,17 @@ void HandleKeypresses(void)
 
 void glInit(void)
 {
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    OGL_SetOrthographicMatrix(COORD_GL_LOWERBOUND_X, COORD_GL_UPPERBOUND_X, COORD_GL_UPPERBOUND_Y, COORD_GL_LOWERBOUND_Y, COORD_GL_LOWERBOUND_Z, COORD_GL_UPPERBOUND_Z);
-    
-    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE); glEnable(GL_COLOR_MATERIAL);
-    
-    glShadeModel(GL_FLAT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    
-    OGL_InitVertexArrays();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	OGL_SetOrthographicMatrix(COORD_GL_LOWERBOUND_X, COORD_GL_UPPERBOUND_X, COORD_GL_UPPERBOUND_Y, COORD_GL_LOWERBOUND_Y, COORD_GL_LOWERBOUND_Z, COORD_GL_UPPERBOUND_Z);
+
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE); glEnable(GL_COLOR_MATERIAL);
+
+	glShadeModel(GL_FLAT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	OGL_InitVertexArrays();
 }
 
 void jiggyInit(void)
@@ -127,19 +129,19 @@ void jiggyInit(void)
 #if _WIN32
 	OGL_InitWGL();
 #endif
-    glInit();
-    
-    InitGeometry();
-    jiggyInitTestLevel();
+	glInit();
+
+	InitGeometry();
+	jiggyInitTestLevel();
 
 }
 
 void jiggyRenderFrame(void)
 {
 #if __APPLE__
-    HandleKeypresses();
+	HandleKeypresses();
 #endif
-	
+
 	/*** Start check if shape position has been achieved ***/
 
 	//distance tracker
@@ -150,15 +152,37 @@ void jiggyRenderFrame(void)
 	std::cout << demo[iterator].x - global_LevelShapes[iterator]->getyPos() << ")";
 	//std::system("clear");
 	*/
-	
+
 	//range of accepted value
-	if ( (global_LevelShapes[iterator]->getxPos() <= demo[iterator].x + 20 && global_LevelShapes[iterator]->getxPos() >= demo[iterator].x - 20) && 
-		 (global_LevelShapes[iterator]->getyPos() <= demo[iterator].y + 20 && global_LevelShapes[iterator]->getyPos() >= demo[iterator].y - 20) ) {
-		
+
+	for (int i = 0; i < global_LevelShapes.size(); i++) {
+
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution <int> dis(-50, 50);
+
+		int randVal = dis(gen);
+		int horVel = randVal;
+
+		randVal = dis(gen);
+		int verVel = randVal;
+
+		randVal = dis(gen);
+		int rot = 5;
+
+		global_LevelShapes[i]->updateGeometry(horVel, verVel, rot);
+
+	}
+
+	int buffer = 20; // change this to adjust the pickyness of position
+
+	if ((global_LevelShapes[iterator]->getxPos() <= demo[iterator].x + buffer && global_LevelShapes[iterator]->getxPos() >= demo[iterator].x - buffer) &&
+		(global_LevelShapes[iterator]->getyPos() <= demo[iterator].y + buffer && global_LevelShapes[iterator]->getyPos() >= demo[iterator].y - buffer)) {
+
 		if (iterator < demo.size() - 1) {
 			iterator++;
 		}
-		
+
 		else {
 			exit(1);
 		}
@@ -166,64 +190,64 @@ void jiggyRenderFrame(void)
 
 	/*** End check if shape position has been achieved ***/
 
-    OGL_ApplyCameraTransformations();
-    OGL_UpdateVertexArrays();
+	OGL_ApplyCameraTransformations();
+	OGL_UpdateVertexArrays();
 }
 
 void jiggyInitTestLevel()
 {
-    
-    JIG::LoadLevel testLevel ("/Users/Ryan/Documents/College-UAF/Classwork/CS202/jiggyProject/level1.txt"); // location of level1.txt
-    
-    for (unsigned i = 0; i < testLevel.size(); ++i)
-    {
-        testLevel.color(i).r += 10.0f/255.0f;
-        testLevel.color(i).g += 15.0f/255.0f;
-        testLevel.color(i).b += 20.0f/255.0f;
-        
-        testLevel.location(i).x += 200;
-        testLevel.location(i).y += 200;
-        
-        testLevel.rotation(i) += 100;
-    }
-    
-    testLevel.renderLevel();
-    
-    
-    global_LevelShapes.push_back(std::make_unique<JIG::ArcSlice>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(1.0f, 1.0f, 1.0f, 1.0f))));
-	global_LevelShapes[0]->submitForRender();
-    
-//    global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(100, 100, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.8f, 1.0f, 1.0f, 1.0f))));
-//    global_LevelShapes[1]->submitForRender();
-//    
-//    global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(-150, 100, 200), 100.0f, 100.0f, 45.0f, JIGMakeColor(0.8f, 0.7f, 1.0f, 1.0f))));
-//    global_LevelShapes[1]->submitForRender();
-//    
-//    global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 300, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.3f, 1.0f, 0.6f, 1.0f))));
-//    global_LevelShapes[1]->submitForRender();
-//    
-    global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 200, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.4f, 0.5f, 0.3f, 1.0f))));
-    global_LevelShapes[1]->submitForRender();
-//    
-//    global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 100, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.1f, 0.8f, 0.7f, 1.0f))));
-//    global_LevelShapes[5]->submitForRender();
-//    
-//	JIG::CollisionDetector col(global_LevelShapes[0], global_LevelShapes);
-//	if (col.isCollided())
-//	{
-//		global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, -200, -200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.1f, 0.8f, 0.7f, 1.0f))));
-//		global_LevelShapes[5]->submitForRender();
-//	}
 
-    
-    
-    
-    //std::unique_ptr<JIG::ArcSlice> aPtr = std::make_unique<JIG::ArcSlice>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 40.0f, 50.0f, 0.0f, JIGMakeColor(0.0f, 0.0f, 0.0f, 1.0f), -1));
-//    global_LevelShapes.push_back(std::make_unique<JIG::Shape>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 200.0f, 360.0f, 0.0f, JIGMakeColor(1.0f, 0.85f, 0.35f, 1.0f))));
-    //global_LevelShapes[0]->submitForRender();
+	JIG::LoadLevel testLevel("/Users/Ryan/Documents/College-UAF/Classwork/CS202/jiggyProject/level1.txt"); // location of level1.txt
+
+	for (unsigned i = 0; i < testLevel.size(); ++i)
+	{
+		testLevel.color(i).r += 10.0f / 255.0f;
+		testLevel.color(i).g += 15.0f / 255.0f;
+		testLevel.color(i).b += 20.0f / 255.0f;
+
+		testLevel.location(i).x += 200;
+		testLevel.location(i).y += 200;
+
+		testLevel.rotation(i) += 100;
+	}
+
+	testLevel.renderLevel();
+
+	global_LevelShapes.push_back(std::make_unique<JIG::ArcSlice>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(1.0f, 1.0f, 1.0f, 1.0f))));
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(100, 100, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.8f, 1.0f, 1.0f, 1.0f))));
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(-100, 100, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.8f, 0.7f, 1.0f, 1.0f))));
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 300, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.3f, 1.0f, 0.6f, 1.0f))));
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 200, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.4f, 0.5f, 0.3f, 1.0f))));
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, 100, 200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.1f, 0.8f, 0.7f, 1.0f))));
+
+	/*Mark Stuff
+	global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(-150, 100, 200), 100.0f, 100.0f, 45.0f, JIGMakeColor(0.8f, 0.7f, 1.0f, 1.0f))));
+	global_LevelShapes[1]->submitForRender();
+	*/
+
+	for (unsigned int i = 0; i < global_LevelShapes.size(); i++) {
+
+		global_LevelShapes[i]->submitForRender();
+
+	}
+
+	//    
+	//	JIG::CollisionDetector col(global_LevelShapes[0], global_LevelShapes);
+	//	if (col.isCollided())
+	//	{
+	//		global_LevelShapes.push_back(std::make_unique<JIG::Rectangle>(JIG::Rectangle(JIGMakePoint(0, -200, -200), 100.0f, 100.0f, 0.0f, JIGMakeColor(0.1f, 0.8f, 0.7f, 1.0f))));
+	//		global_LevelShapes[5]->submitForRender();
+	//	}
+
+
+
+
+	//std::unique_ptr<JIG::ArcSlice> aPtr = std::make_unique<JIG::ArcSlice>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 40.0f, 50.0f, 0.0f, JIGMakeColor(0.0f, 0.0f, 0.0f, 1.0f), -1));
+	//    global_LevelShapes.push_back(std::make_unique<JIG::Shape>(JIG::ArcSlice(JIGMakePoint(0, 0, 200), 200.0f, 360.0f, 0.0f, JIGMakeColor(1.0f, 0.85f, 0.35f, 1.0f))));
+	//global_LevelShapes[0]->submitForRender();
 }
 
 void jiggyLevelUpdate(void)
 {
-    
+
 }
